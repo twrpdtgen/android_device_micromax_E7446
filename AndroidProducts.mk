61db081976d7fbac00381caed356955ae9b6ce29
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-10 08:05:46 +0100

    E7446: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: micromax Model: E7446 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_E7446.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_E7446-user \
+    omni_E7446-userdebug \
+    omni_E7446-eng
